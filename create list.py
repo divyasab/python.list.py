bbcf6bcf361fa49e30636b4a6313491580897620
Divya sable <103986474+divyasab@users.noreply.github.com>
2022-12-16 16:03:55 +0530

    Add files via upload

---
+++ create list.py
@@ -0,0 +1,79 @@
+a=int(input("enter the number"))
+i=1
+b=[]
+while i<=(a):
+    n=(input("enter the number"))
+    b.append(n)
+    i=i+1
+print(b) 
+
+# a=35
+# b=40
+# c=20
+# a=23
+# b=12
+# c=25
+# b=20
+# print(a+b+c)
+
+# a=5
+# b="jyo"
+# c=str(a)
+# print(b+c)
+
+
+
+
+# a=35
+# b=40
+# c=20
+# a=23
+# b=12
+# c=25
+# b=20
+# print(a+b+c)
+
+
+# "div"=123
+# _@4=56
+# divya=_@20
+# if=True
+# True=201
+
+
+# _123=false
+
+# a=10
+# b="5"
+# c=12+Oj
+# d=a+c
+# e=str(a)
+# f=str(c)
+# print(e+e+f+c)
+
+# a=5
+# b="div"
+# print(a+b)
+
+# a=5
+# b="div"
+# print(a-b)
+
+# a=5
+# b="div"
+# print(a*b)
+
+
+
+# a=35
+# b=40
+# c=20
+# a=23
+# b=12
+# c=25
+# b=20
+# print(a+b+c)
+
+# a=170
+# b=2
+# print(a/b)
