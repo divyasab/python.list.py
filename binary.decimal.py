898bacbf4678b94692464547fd95c9339bedc49f
Divya sable <103986474+divyasab@users.noreply.github.com>
2022-12-16 15:56:39 +0530

    Add files via upload

---
+++ binary.decimal.py
@@ -0,0 +1,44 @@
+# a=[1,0,0,1,1,0,1,1]
+# i=-1
+# b=0
+# sum=0
+# while i>=-len(a):
+#     sum=sum+a[i]*2**b
+#     b=b+1
+#     i=i-1
+# print(sum)
+
+# a="my name is divya"
+# print("my\tname\nis\ndivya")
+
+# i=1
+# while i<len(a):
+
+# a=[1,0,0,1,1,0]
+# i=-1
+# b=0
+# sum=0
+# while i>=-len(a):
+#     sum=sum+a[i]*2**b
+#     b=b+1
+#     i=i-1
+# print(sum)
+
+# def fun():
+#     a=int(input("enter"))
+#     b=int(input("enter"))
+#     c=(a+b)
+#     print(c)
+# fun()
+
+# def fun(a,b):
+#     c=a+b
+#     print(c)
+# fun(5,7)
+
+# def fun():
+#     a=int(input("enter"))
+#     b=int(input("enter"))
+#     c=a+b
+#     return(c)
+# fun()
